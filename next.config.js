6eeaee259b3011522496d9d65acc83b468a0b2c2
collze7 <121809317+collze7@users.noreply.github.com>
2026-04-17 15:25:17 -0400

    Update next.config.js for image optimization
    Remove unoptimized images setting and update remote hostname.

--- next.config.js
+++ next.config.js
@@ -3,17 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'commitments.site',
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
